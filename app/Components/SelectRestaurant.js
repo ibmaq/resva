0821e1b755ddeb260b106948229843c0e3eac88d
Muhammad Ibrahim <ibrahimaq99@gmail.com>
2023-12-16 09:22:36 +0500

    minor enhancements

--- app/Components/SelectRestaurant.js
+++ app/Components/SelectRestaurant.js
@@ -64,7 +64,7 @@ export default function SelectRestaurant({ setCurrentPage }) {
                     Add to bag
                   </button> */}
                 </div>
-                <button
+                {/* <button
                   className="flex-none flex items-center justify-center w-9 h-9 rounded-md text-slate-300 border border-slate-200"
                   type="button"
                   aria-label="Like"
@@ -81,7 +81,7 @@ export default function SelectRestaurant({ setCurrentPage }) {
                       d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
                     />
                   </svg>
-                </button>
+                </button> */}
               </div>
             </form>
           </div>
